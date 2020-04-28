448519f4872a8e437869ee39d961dc2b4aee40ed
alexmerryman <alexandermerryman2018@u.northwestern.edu>
2020-04-27 23:21:51 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import requests
+import os
+import facebook
+
+
+fb_token = os.environ['FB_ACCESS_TOKEN']
+graph = facebook.GraphAPI(access_token=fb_token, version=3.1)
+
+places = graph.search(type='place',
+                      # center='33.666855,-115.871783',
+                      # distance=100000,
+                      fields='name,id,checkins,location,category_list,differently_open_offerings,hours,temporary_status',
+                      q="riverside california"
+                      )
+
+for place in places['data']:
+    try:
+        print(place['name'], place['location'].get('zip'), place['hours'],
+              place['temporary_status'], place['differently_open_offerings'], place['category_list'])
+    except KeyError:
+        print('ERROR ---- ', place['name'], place['location'].get('zip'), place['checkins'])
+
