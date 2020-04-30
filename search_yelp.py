2afd6f3adb9c926606aee129b693ad33a98a08b8
alexmerryman <alexandermerryman2018@u.northwestern.edu>
2020-04-29 23:57:08 -0600

    - Refined Yelp search & business detail retrieval to aggregate if more than 50 results found. - Lat/long search still not working, seems to be issue with API.

--- search_yelp.py
+++ search_yelp.py
@@ -3,6 +3,7 @@ import numpy as np
 import requests
 import json
 import os
+from datetime import datetime
 # import yelp
 from yelp.client import Client
 
@@ -16,35 +17,39 @@ client = Client(yelp_api_key)
 search_headers = {'Authorization': 'Bearer %s' % yelp_api_key}
 
 
-def business_search_yelp():
-    # TODO: add parameters as input arguments to this function
+def business_search_yelp(search_params):
+    search_url = 'https://api.yelp.com/v3/businesses/search'
 
-    url = 'https://api.yelp.com/v3/businesses/search'
-
-    lat = 33.711247
-    long = -115.984767
-    # TODO: Get lat/long search params to work (why isn't it?)
-    params = {
-        'location': 'Riverside County, CA'
-        # 'latitude:': lat
-        # ,'longitude': long
-        # ,'radius': 40000
-        ,'limit': 50
-        # ,'offset': 50
-    }
-
-    response = requests.get(url, params=params, headers=search_headers)
+    response = requests.get(search_url, params=search_params, headers=search_headers)
+    # print(response.request.url)
     # print(response.text)
 
     # proceed only if the status code is 200
-    # TODO: Add try/except clause to deal with unexpected response behaviors
-    print('The status code is {}'.format(response.status_code))
+    if response.status_code == 200:
+        # search_results_json = json.loads(response.text)
+        search_results_df = pd.DataFrame.from_dict(response.json()['businesses'])
+        return search_results_df
 
-    # search_results_json = json.loads(response.text)
+    else:
+        print('Non-200 status code: {}'.format(response.status_code))
+        return None  # TODO: ???
 
-    search_results_df = pd.DataFrame.from_dict(response.json()['businesses'])
 
-    return search_results_df
+def loop_offset_search(search_params):
+    search_dfs_list = [business_search_yelp(search_params)]
+    offset_mult = 1
+    while search_dfs_list[-1].shape[0] == 50:
+        print('---------------------- Looping through results... ----------------------')
+        print('# dfs in list:', len(search_dfs_list))
+        search_params['offset'] = 50 * offset_mult
+        print('# results:', search_params['offset'])
+        search_dfs_list.append(business_search_yelp(search_params))
+        offset_mult += 1
+
+    all_search_results_df = pd.concat(search_dfs_list)
+    # TODO: Dedupe DF just in case there truly are exactly 50 results?
+
+    return all_search_results_df
 
 
 def business_details_yelp(yelp_id):
@@ -54,10 +59,11 @@ def business_details_yelp(yelp_id):
 
     details_df = pd.DataFrame.from_dict([business_details_response.json()])
 
+    # TODO: Explain how these fields can indicate great confidence that profile has been updated recently?
     # details_dict = {'id': business_details_response.json()['id'],
     #                 'name': business_details_response.json()['name'],
     #                 'alias': business_details_response.json()['alias'],
-    #                 'is_claimed': business_details_response.json()['is_claimed'], # greater confidence that profile has been updated
+    #                 'is_claimed': business_details_response.json()['is_claimed'],
     #                 'review_count': business_details_response.json()['review_count'],
     #                 'categories': business_details_response.json()['categories'],
     #                 'is_closed': business_details_response.json()['is_closed'],
@@ -66,9 +72,6 @@ def business_details_yelp(yelp_id):
     #                 }
 
     return details_df
-
-
-# print(business_details_yelp('sQ-dABqseCrb07yjF3CbDA'))
 
 
 def aggregate_details_from_search(search_results_df):
@@ -82,28 +85,62 @@ def aggregate_details_from_search(search_results_df):
     return all_search_details_df
 
 
-search_riverside_ca = business_search_yelp()
-details_riverside_ca = aggregate_details_from_search(search_riverside_ca)
-
-print(details_riverside_ca.columns)
-print(details_riverside_ca.head())
-print(details_riverside_ca[['name', 'id', 'location', 'is_closed', 'is_claimed', 'transactions']])
-
-
 def extract_zip_code(details_df_row):
-    if len(details_df_row['location']['zip_code']) < 1:
+    if not isinstance(details_df_row['location']['zip_code'], str):
+        return str(details_df_row['location']['zip_code'])
+    elif len(details_df_row['location']['zip_code']) < 1:
         return None
     else:
         return details_df_row['location']['zip_code']
 
 
-details_riverside_ca['zip_code'] = details_riverside_ca.apply(lambda row: extract_zip_code(row), axis=1)
-print(details_riverside_ca[['id', 'name', 'zip_code']])
+def run_full_search(search_params):
+    print("Searching for results matching '{}'...".format(search_params['location']))
+    search_df = loop_offset_search(search_params)
+    print('\nGetting details on all matching businesses (may take a few minutes)...')
+    details_df = aggregate_details_from_search(search_df)
+    # TODO: Add search params to column? Or at least location?
+    print(details_df.columns)
+    print(details_df.head())
+    print(details_df[['name', 'id', 'location', 'is_closed', 'is_claimed', 'transactions']])
+    details_df['zip_code'] = details_df.apply(lambda row: extract_zip_code(row), axis=1)
+    details_df['api_call_datetime'] = datetime.now()
+    print(details_df[['id', 'name', 'zip_code']])
+
+    return details_df
+
+
+# TODO: Get lat/long search params to work (why isn't it??)
+#  Getting error: "{"error": {"code": "VALIDATION_ERROR", "description": "Please specify a location or a latitude and longitude"}}"
+lat = float(33.769327)
+long = float(-116.312849)
+rad = int(4000)
+params_lat_long = {
+    'latitude:': lat,
+    'longitude': long,
+    'radius': rad
+}
+
+
+
+palm_springs_params = {
+    'location': 'palm springs, CA',
+    'limit': 50
+}
+
+palm_springs_df = run_full_search(palm_springs_params)
+print(palm_springs_df.shape)
+print(palm_springs_df.columns)
+print(palm_springs_df)
+
+palm_springs_df.to_pickle("./palm_springs.pkl")
 
 # TODO: Sample and collect a few cities/locations, aggregate into database
 
-# TODO: Cross-reference businesses with FB, Google, etc? (by name, location?)
-# TODO: Collect date/time of API call to track historical trends
+# TODO: Cross-reference businesses with FB, Google, etc? (by name, location?) Yelp business match: https://www.yelp.com/developers/documentation/v3/business_match
 # TODO: Store somewhere -- USDR database?
 # TODO: Business closure rate, modified hours rate // by zipcode, category
 # TODO: Identify Opportunity Zones (by zipcode)
+
+
+# TODO: Rename this file to something more general
