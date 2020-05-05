c0c3adf0189daa655683f35724b7a70bcde72c5e
alexmerryman <alexandermerryman2018@u.northwestern.edu>
2020-05-04 22:00:44 -0600

    Still working on Facebook search -- generally working, but need to get data into DF.
    Also, running into issue with Yelp search where it appears location/zip code field(s) may be coming in as "nan" -- trying to fix.

--- search_facebook.py
+++ search_facebook.py
@@ -5,42 +5,55 @@ import facebook
 
 
 fb_token = os.environ['FB_ACCESS_TOKEN']  # this access token will expire on June 29, 2020
+
 try:
     graph = facebook.GraphAPI(access_token=fb_token, version=3.1)
 except:
     print('Error: Unable to initialize Facebook Graph API access.')
 
-try:
-    places = graph.search(type='place',
-                          # center='33.666855,-115.871783',
-                          # distance=100000,
-                          fields='name,id,checkins,location,category_list,differently_open_offerings,hours,temporary_status',
-                          q="palm springs, CA"
-                          )
-except:
-    print('Error: Facebook Graph API search invalid.')
 
-# TODO: Create DF schema, then fill it with data, because some records don't have every field
+def business_search_facebook():  # TODO: add params
+    try:
+        places = graph.search(type='place',
+                              # center='33.666855,-115.871783',
+                              # distance=100000,
+                              fields='name,id,checkins,location,category_list,differently_open_offerings,hours,temporary_status',
+                              q="palm springs, CA"
+                              )
+    except:
+        print('Error: Facebook Graph API search invalid.')
+
+    # TODO: Transform into DF?
+
+
+
+# TODO: Functionize following steps, use output from business_search_facebook as input
+
+# TODO: Issue: if a record has no data for a particular field, it will simply return the record with neither the
+#  value NOR the key. Solution: create DF schema, then fill it with data/values or nulls as they come up.
 for p in places['data']:
-    # print(p)
-    # TODO: Add try/except
-    info = graph.get_object(id=p['id'], fields="id,name,description,category_list,checkins,location,\
-                                               temporary_status,differently_open_offerings,hours,\
-                                               is_always_open,is_permanently_closed,\
-                                               is_verified,parking,payment_options,\
-                                               restaurant_services")
-    print(info)
-    # p['id']
-    if 'temporary_status' in p.keys():
-        print(p['id'], p['temporary_status'])
-    # try:
-    #     print(p['name'], p['location'].get('zip'), p['hours'],
-    #           p['temporary_status'], p['differently_open_offerings'], p['category_list'])
-    # except KeyError:
-    #     print('ERROR ---- ', p['name'], p['location'].get('zip'), p['checkins'])
+    try:
+        info = graph.get_object(id=p['id'], fields="id,name,description,category_list,checkins,location,\
+                                                   temporary_status,differently_open_offerings,hours,\
+                                                   is_always_open,is_permanently_closed,\
+                                                   is_verified,parking,payment_options,\
+                                                   restaurant_services")
+    except:
+        info = None
+        print('Error: invalid API call.')
 
-# TODO: if KeyError (value does not exist), fill with null
+    full_keys = ['id', 'name', 'description', 'category_list', 'checkins', 'location', 'temporary_status',
+                 'differently_open_offerings', 'hours', 'is_always_open', 'is_permanently_closed', 'is_verified',
+                 'parking', 'payment_options', 'restaurant_services']
+
+    missing_fields = full_keys - p.keys()
+    for m in missing_fields:
+        info[m] = None
+
+    print(info)
+    # if 'temporary_status' in p.keys():
+    #     print(p['id'], p['temporary_status'])
+
 
 # TODO: Same as Yelp search -- for each ID retrieved by FB search, use Place Information endpoint to get more details
 
-# for each place ID, grab open status (differently_open_offerings, hours, temporary_status)
